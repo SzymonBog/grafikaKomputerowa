1e896d12fa48e0f06b67dc53fcd491512b776695
SzymonBog <92025573+SzymonBog@users.noreply.github.com>
2023-10-08 12:39:54 +0200

    dokoncz
    utworz raport

--- lab1/main.py
+++ lab1/main.py
@@ -42,5 +42,21 @@ print("trzeci wyraz:", dane_obrazka[0][99])
 print("***************************************")  # wyniki sa ok
 
 # 5
-tb = np.loadtxt("inicjaly.txt", dtype=np.bool)
+nowe_dane_obrazka = np.loadtxt("inicjaly.txt", dtype=np.bool_)
+stare_dane_obrazka = np.asarray(obrazek)  # stary
+
+if stare_dane_obrazka.dtype == nowe_dane_obrazka.dtype and stare_dane_obrazka.shape == nowe_dane_obrazka.shape and stare_dane_obrazka.size == nowe_dane_obrazka.size and stare_dane_obrazka.ndim == nowe_dane_obrazka.ndim and stare_dane_obrazka.itemsize == nowe_dane_obrazka.itemsize:
+    print("Informacje o tych tablicach sa takie same")
+else:
+    print("Informacje o tych tablicach NIE sa takie same")
+
+# 6
+nowe_dane_obrazka = np.loadtxt("inicjaly.txt", dtype=np.uint8)
+
+if stare_dane_obrazka.dtype == nowe_dane_obrazka.dtype and stare_dane_obrazka.shape == nowe_dane_obrazka.shape and stare_dane_obrazka.size == nowe_dane_obrazka.size and stare_dane_obrazka.ndim == nowe_dane_obrazka.ndim and stare_dane_obrazka.itemsize == nowe_dane_obrazka.itemsize:
+    print("Informacje o tych tablicach sa takie same")
+else:
+    print("Informacje o tych tablicach NIE sa takie same")
+# informacje nie sa takie same poniewaz typy danych sa rozne(int != bool)[roznia sie tylko w: dtype]
+Image.fromarray(nowe_dane_obrazka).show()
 
